7df67c743fdb0648bdf3610069ad52d940912793
Tiến <letien3413@gmail.com>
2024-12-04 18:41:49 +0700

    add template admin

---
+++ resources/views/backend/layout_admin/headonly.blade.php
@@ -0,0 +1,7 @@
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>{{ $title ?? 'Website quản lý thiết bị công nghệ' }}</title>
+    <link rel="shortcut icon" type="image/png" href="{{ asset('backend/assets/images/logos/favicon.png') }}" />
+    <link rel="stylesheet" href="{{ asset('backend/assets/css/styles.min.css') }}" />
+  </head>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\Backend\DashboardController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('admin')->group(function () {
+    Route::get('/', [DashboardController::class, 'index']);
 });
